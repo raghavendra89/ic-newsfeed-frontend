a756920d2e5583722c265ff5972d33fdc356cf87
Raghavendra <nraghavendra89@gmail.com>
2024-12-22 23:44:15 +0530

    WIP: Layout setup.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
-import './index.css'
+import '@tabler/core/src/scss/tabler.scss';
+import '@tabler/core';
+import './App.css'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
-
-        <Route path="/my-feed" element={<App />} />
-        <Route path="/preferences" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="/my-feed" element={<App />} />
+          <Route path="/preferences" element={<App />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
